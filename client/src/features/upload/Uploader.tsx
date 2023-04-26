da3b17c8c41b54eb77eb358e69f6bb7177243e7a
Trung Hieu <hieu@ltv.dev>
2023-04-26 15:14:44 +0700

    Using React Router Dom

---
+++ client/src/features/upload/Uploader.tsx
@@ -0,0 +1,5 @@
+import { Typography } from '@mui/material';
+
+export default function Uploader() {
+	return <Typography variant="h2">Uploader Page</Typography>;
+}
--- client/src/layout/App.tsx
+++ client/src/layout/App.tsx
@@ -5,10 +5,17 @@ import {
 	ThemeProvider,
 	createTheme,
 } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
+
 import Catalog from '../features/catalog/Catalog';
 import Header from './Header';
+import HomePage from '../features/home/HomePage';
+import AboutPage from '../features/about/AboutPage';
+import ContactPage from '../features/contact/ContactPage';
+import Uploader from '../features/upload/Uploader';
+import ProductDetail from '../features/catalog/ProductDetail';
 
 function App() {
 	const [darkMode, setDarkMode] = useState(false);
@@ -25,7 +32,14 @@ function App() {
 				<CssBaseline />
 				<Header onSetDarkMode={setDarkMode} darkMode={darkMode} />
 				<Container>
-					<Catalog />
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="catalog" element={<Catalog />} />
+						<Route path="catalog/:productId" element={<ProductDetail />} />
+						<Route path="about" element={<AboutPage />} />
+						<Route path="contact" element={<ContactPage />} />
+						<Route path="upload" element={<Uploader />} />
+					</Routes>
 				</Container>
 			</ThemeProvider>
 		</>
