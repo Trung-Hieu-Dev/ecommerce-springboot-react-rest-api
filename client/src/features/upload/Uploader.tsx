8dd2cd3af20fdcafb51feafcba5ff599d722ed8e
Trung Hieu <hieu@ltv.dev>
2023-04-27 16:32:45 +0700

    Upload File

--- client/src/features/upload/Uploader.tsx
+++ client/src/features/upload/Uploader.tsx
@@ -1,5 +1,22 @@
-import { Typography } from '@mui/material';
+import { Input, Typography } from '@mui/material';
 
 export default function Uploader() {
-	return <Typography variant="h2">Uploader Page</Typography>;
+	const uploadFile = (e: any) => {
+		e.preventDefault();
+		const formData = new FormData();
+		formData.append('file', e.target.files[0]);
+		fetch('http://localhost:8080/api/file/upload', {
+			method: 'post',
+			body: formData,
+		}).then((res) => {
+			alert('Upload successfully!');
+		});
+	};
+
+	return (
+		<>
+			<Typography variant="h2">Uploader Page</Typography>
+			<Input type="file" onChange={uploadFile} />
+		</>
+	);
 }
