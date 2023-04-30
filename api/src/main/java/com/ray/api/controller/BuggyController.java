56d237d48b53802ddec95f43bd351201bb00727a
Trung Hieu <hieu@ltv.dev>
2023-04-30 10:33:56 +0700

    Backend: Fake Error for Testing

--- api/src/main/java/com/ray/api/controller/BuggyController.java
+++ api/src/main/java/com/ray/api/controller/BuggyController.java
@@ -1,9 +1,11 @@
 package com.ray.api.controller;
 
+import com.ray.api.dto.HttpResponse;
 import com.ray.api.entity.Buggy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.validation.Valid;
 
@@ -11,9 +13,35 @@ import javax.validation.Valid;
 @RequestMapping("/api/buggy")
 @CrossOrigin("http://localhost:3000")
 public class BuggyController {
+
     @PostMapping("validate-error")
     public ResponseEntity<Buggy> createValidationError(@Valid @RequestBody Buggy buggy) {
         System.out.println(buggy);
         return new ResponseEntity<>(buggy, HttpStatus.CREATED);
+    }
+
+    // fake errors to test
+    ///404 fake error
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<HttpResponse> noHandlerFoundException() {
+        return createHttpResponse(HttpStatus.NOT_FOUND, "There is no mapping for this URL");
+
+    }
+
+    ///500 fake error
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<HttpResponse> internalServerErrorException(Exception ex) {
+        return createHttpResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+    }
+
+    /// reused method
+    private ResponseEntity<HttpResponse> createHttpResponse(HttpStatus httpStatus, String message) {
+        HttpResponse httpResponse = new HttpResponse(
+                httpStatus.value(),
+                httpStatus,
+                httpStatus.getReasonPhrase(),
+                message
+        );
+        return new ResponseEntity<>(httpResponse, httpStatus);
     }
 }
--- api/src/main/java/com/ray/api/exception/Exceptionhandling.java
+++ api/src/main/java/com/ray/api/exception/Exceptionhandling.java
@@ -9,15 +9,43 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 public class Exceptionhandling {
+
+    //404 error
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<HttpResponse> noHandlerFoundException() {
-        return new ResponseEntity<>(
-                new HttpResponse(
-                    HttpStatus.NOT_FOUND.value(),
-                    HttpStatus.NOT_FOUND,
-                    HttpStatus.NOT_FOUND.getReasonPhrase(),
-                    "There is no mapping for this URL"),
-                HttpStatus.NOT_FOUND
-        );
+//        return new ResponseEntity<>(
+//                new HttpResponse(
+//                    HttpStatus.NOT_FOUND.value(),
+//                    HttpStatus.NOT_FOUND,
+//                    HttpStatus.NOT_FOUND.getReasonPhrase(),
+//                    "There is no mapping for this URL"),
+//                HttpStatus.NOT_FOUND
+//        );
+        return createHttpResponse(HttpStatus.NOT_FOUND, "There is no mapping for this URL");
+
+    }
+
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<HttpResponse> internalServerErrorException(Exception ex) {
+//        return createHttpResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+
+//        return new ResponseEntity<>(
+//                new HttpResponse(
+//                        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+//                        HttpStatus.INTERNAL_SERVER_ERROR,
+//                        HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+//                        ex.getMessage()),
+//                HttpStatus.INTERNAL_SERVER_ERROR
+//        );
+//    }
+
+    private ResponseEntity<HttpResponse> createHttpResponse(HttpStatus httpStatus, String message) {
+        HttpResponse httpResponse = new HttpResponse(
+                                        httpStatus.value(),
+                                        httpStatus,
+                                        httpStatus.getReasonPhrase(),
+                                        message
+                                    );
+        return new ResponseEntity<>(httpResponse, httpStatus);
     }
 }
